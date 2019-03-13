1473ae2ef7b0911d497d1500a6ba2a4c223f980f
Daniel Dao <guangdanny@gmail.com>
2019-03-14 10:44:42 +1100

    add screenshoot, password login

--- app.py
+++ app.py
@@ -3,24 +3,27 @@ from tkintertoy import Window
 import smtplib
 import email
 
+password= 'Qn7Fxa9WRUwbz7:'
+
 #Connect to server
-server = smtplib.SMTP('smtp.gmail.com:587')
+server = smtplib.SMTP('smtp.outlook.com:587')
 server.starttls()
-server.login('guangdanny@gmail.com', 'mnrjqllfdihmicer')
 
 #Set up GUI
 gui = Window()
 gui.setTitle('Pick two ports for your NP class!')
 gui.addEntry('stu_no', 'Type in your student number')
+gui.addEntry('pass', 'Type in your student account\'s password')
 gui.addEntry('port_1', 'Pick port 1')
 gui.addEntry('port_2', 'Pick port 2')
 gui.addLabel('message', '')
 gui.addButton('commands')
 gui.plot('stu_no', row=0)
-gui.plot('port_1', row=1)
-gui.plot('port_2', row=2)
-gui.plot('message', row=3)
-gui.plot('commands', row=4, pady=10)
+gui.plot('pass', row=1)
+gui.plot('port_1', row=2)
+gui.plot('port_2', row=3)
+gui.plot('message', row=4)
+gui.plot('commands', row=5, pady=10)
 
 #spreadsheet information
 SPREADSHEET_ID = "1mn93Hl0zT7Cb2YaPryj523IQEzelVa1HWLrR3ZxrJRU" # <Your spreadsheet ID>
@@ -36,12 +39,17 @@ df = wks.get_as_df(has_header=True, index_colum=1, empty_value="")
 
 
 #Check if student number is valid
-def checkStuNo(student_no, port_1, port_2):
+def checkStuNo(student_no, port_1, port_2, password):
     # working
     if (student_no not in df.index):
         gui.set('message', "Your student number is not correct")
     else:
-        addPorts(student_no, port_1, port_2)
+        try:
+            server.login(student_no + '@student.rmit.edu.au', password)
+        except smtplib.SMTPAuthenticationError:
+            gui.set('message', "Your password is incorrect")
+        else:
+            addPorts(student_no, port_1, port_2)
 
 #Add ports
 def addPorts(student_no, port_1, port_2):
@@ -60,21 +68,19 @@ def addPorts(student_no, port_1, port_2):
         gui.set('message', "Pick ports successfully")
         # Message for email
         msg = '\r\n' + 'Student: ' + student_no + ' picks: ' + port_1 + ' and ' + port_2
-        server.sendmail('guangdanny@gmail.com', 'guangdanny@gmail.com', msg)
-
-
+        server.sendmail(student_no + '@student.rmit.edu.au', 'guangdanny@gmail.com', msg)
+        server.quit()
 while True:
     gui.waitforUser()
     if gui.content:
-        if (gui.get('stu_no') and gui.get('port_1') and gui.get('port_2')):
-            checkStuNo(gui.get('stu_no'), gui.get('port_1'), gui.get('port_2'))
+        if (gui.get('stu_no') and gui.get('port_1') and gui.get('port_2') and gui.get('pass')):
+            checkStuNo(gui.get('stu_no'), gui.get('port_1'), gui.get('port_2'), gui.get('pass'))
         else:
             gui.set('message', "Please fill every boxes")
     else:
         break
 
 
-server.quit()
 #Write to google spreadsheet
 wks.set_dataframe(df,(1,1), copy_index=True)
 header = wks.cell('A1')
